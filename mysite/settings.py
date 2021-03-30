0ce1e468cf2f44b19c74a0d5666454355d5a7d3b
marku <markusvynycyus@hotmail.com>
2021-03-29 21:00:55 -0300

    Deploy Allowed_Hots

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+import django_heroku
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,5 +129,4 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile')
 
 # Configure Django App for Heroku.
-import django_heroku
 django_heroku.settings(locals())
