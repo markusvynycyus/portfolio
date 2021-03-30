899c00c4c7fe3cff12afc6d1c74860f30cab4044
marku <markusvynycyus@hotmail.com>
2021-03-29 21:07:49 -0300

    Deploy Allowed_Hots

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,13 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '@2s(*n8+6=y8b6x=awv)thi4lwxft(d2d8-atagw_ew=nvt#j3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = os.environ.get('DEBUG') == True
+DEBUG = True
+#DEBUG = os.environ.get('DEBUG') == True
 
 
 
-#ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['https://portfolio-venicios.herokuapp.com']
+ALLOWED_HOSTS = ['*']
+#ALLOWED_HOSTS = ['https://portfolio-venicios.herokuapp.com']
 
 
 
@@ -111,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt_br'
 
 TIME_ZONE = 'UTC'
 
