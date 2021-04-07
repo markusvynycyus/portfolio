527bfc24ef17c461100c7c03bb3bb93fd4d50998
marku <markusvynycyus@hotmail.com>
2021-04-07 20:01:25 -0300

    Alteracao no Procfile

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,10 +81,22 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+#}
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfolio',
+        'USER': 'venicios',
+        'PASSWORD': '4153791',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
